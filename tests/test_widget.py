1070468349b741e99a6825756bc0aed4497c754e
Alex Nikiforov <alex.kifir95@yandex.ru>
2024-06-12 13:22:31 +0300

    add DocStrings in test functions (mask and widget)

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,46 +3,55 @@ from src.widget import mask_card_number, date_conversion
 
 
 def test_mask_card_type_platinum():
+    """Функция, тестирующая заданное значение банковской карты Visa Platinum с ожидаемым на выходе"""
     new_card_type_platinum: str = "Visa Platinum 6831982476737658"
     assert mask_card_number(new_card_type_platinum) == "Visa Platinum 6831 98** **** 7658"
 
 
 def test_card_type_master():
+    """Функция, тестирующая заданное значение банковской карты Master Card с ожидаемым на выходе"""
     new_card_type_master: str = "Master Card 8541225633574896"
     assert mask_card_number(new_card_type_master) == "Master Card 8541 22** **** 4896"
 
 
 def test_card_type_classic():
+    """Функция, тестирующая заданное значение банковской карты Visa Classic с ожидаемым на выходе"""
     new_card_type_classic: str = "Visa Classic 7589885622154877"
     assert mask_card_number(new_card_type_classic) == "Visa Classic 7589 88** **** 4877"
 
 
 def test_card_type_gold():
+    """Функция, тестирующая заданное значение банковской карты Visa Gold с ожидаемым на выходе"""
     new_card_type_gold: str = "Visa Gold 3654775899651233"
     assert mask_card_number(new_card_type_gold) == "Visa Gold 3654 77** **** 1233"
 
 
 def test_account_number():
+    """Функция, тестирующая заданное значение банковского счета с ожидаемой маской на выходе"""
     new_account_info: str = "Счет 73654108430135874305"
     assert mask_card_number(new_account_info) == "Счет **4305"
 
 
 @pytest.fixture
 def card_type():
+    """Фикстура содержащая функцию, которая возвращает заданный номер карты"""
     return "Visa Platinum 4568452421454868"
 
 
 @pytest.fixture
 def account():
+    """Фикстура содержащая функцию, которая возвращает заданный номер счета"""
     return "Счет 52454568521020142561"
 
 
 def test_masks(card_type, account):
+    """Функция, тестирующая заданный номер карты(счета) с ожидаемой маской на выходе"""
     assert mask_card_number(card_type) == "Visa Platinum 4568 45** **** 4868"
     assert mask_card_number(account) == "Счет **2561"
 
 
 def test_date():
+    """Функция, тестирующая входные параметры даты с ожидаемыми после конвертации"""
     new_date_info_1: str = "2014-12-05"
     assert date_conversion(new_date_info_1) == "05.12.2014"
     new_date_info_2: str = "2018 02 04"
